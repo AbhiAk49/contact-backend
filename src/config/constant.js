24818892be935bc86e8dfb4520f6bee28b6ea798
Abhishek <abhiak49kumar@gmail.com>
2025-09-20 16:05:38 +0530

    cookie var fir fit

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,4 +1,11 @@
 const AUTH_COOKIE = '__contact_app.session';
 const AUTH_COOKIE_REFRESH = '__contact_app.refresh';
 const OAUTH_GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token';
-module.exports = { AUTH_COOKIE, AUTH_COOKIE_REFRESH, OAUTH_GOOGLE_TOKEN_URL };
+
+const AUTH_COOKIE_F = '__fit_app.session';
+const AUTH_COOKIE_REFRESH_F = '__fit_app.refresh';
+const OAUTH_GOOGLE_TOKEN_URL_F = 'https://oauth2.googleapis.com/token';
+module.exports = {
+  AUTH_COOKIE, AUTH_COOKIE_REFRESH, OAUTH_GOOGLE_TOKEN_URL,
+  AUTH_COOKIE_F, AUTH_COOKIE_REFRESH_F, OAUTH_GOOGLE_TOKEN_URL_F
+};
